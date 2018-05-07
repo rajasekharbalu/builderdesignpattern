d4d54b3ef2b52c34081c54ae653eb5f0944040f5
Balu <balu4440@gmail.com>
2018-05-07 16:43:55 +0530

    updated balance

--- builderpattern/src/main/java/com/balu/builderpattern/Test.java
+++ builderpattern/src/main/java/com/balu/builderpattern/Test.java
@@ -5,7 +5,7 @@ import com.balu.builderpattern.builder.AccountBuilder;
 
 public class Test {
 	public static void main(String[] args) {
-		SBIAccount account = new AccountBuilder(12346678).withAccountHolder("Srinicvasrao ").atBranch("Hyderabad")
+		SBIAccount account = new AccountBuilder(12346678).withAccountHolder("Srinu").atBranch("Chennai")
 				.openingBalance(80000.00).atRate(2.50).build();
 		System.out.println(account);
 	}
